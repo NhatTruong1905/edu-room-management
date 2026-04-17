b436c46d3857aec1136af00361b92f204da0ee84
icondenimStore <tn696199@gmail.com>
2026-04-17 17:39:19 +0700

    chore: move sensitive credentials to .env file

--- eduroomapp/__init__.py
+++ eduroomapp/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = '&(^&*^&*^U*HJBJKHJLHKJHK&*%^&5786985646858'
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root%40123@localhost/eduroomdb?charset=utf8mb4"
+app.secret_key = os.environ.get('SECRET_KEY')
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 app.config["PAGE_SIZE"] = 6
 
